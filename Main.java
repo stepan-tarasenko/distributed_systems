befbe82603cbfe7ed7acc2339f9ba24a33347098
Stepan Tarasenko <st.a.tarasenko@gmail.com>
2021-02-08 00:12:06 +0200

    Make some local changes
    Signed-off-by: Stepan Tarasenko <st.a.tarasenko@gmail.com>

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public static void main(String... args) {
 	System.out.print("Hello, Space!");
-	System.out.println("Hello" != new String("Helo").toString().intern());
+	System.out.println("Hello" == new StringBuilder("Hello").toString().intern() ^ true);
+	// Add some comments
 }
