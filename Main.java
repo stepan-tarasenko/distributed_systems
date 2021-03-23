83c38c68256b42616baf8da9fa2361bfa08b98e2
Stepan Tarasenko <st.a.tarasenko@gmail.com>
2021-03-23 21:07:07 +0200

    Remove Main.java

--- Main.java
+++
@@ -1,5 +0,0 @@
-public static void main(String... args) {
-	System.out.print("Hello, Space!");
-	System.out.println("Hello" == new StringBuilder("Hello").toString().intern() ^ true);
-	// Add some comments
-}
