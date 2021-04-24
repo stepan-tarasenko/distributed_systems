b3a5f82191c9191ec92c129cef18a6adb79cee10
Stepan Tarasenko <st.a.tarasenko@gmail.com>
2021-04-24 18:21:30 +0300

    publisher and subscriber

---
+++ rabbitmq/src/main/java/Subscriber.java
@@ -0,0 +1,24 @@
+import com.rabbitmq.client.*;
+
+public class Subscriber {
+    private static final String EXCHANGE_NAME = "pub-sub-queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+}
