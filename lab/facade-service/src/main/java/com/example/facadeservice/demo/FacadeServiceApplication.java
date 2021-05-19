3b0b171275c03f604ed59e1479cd270c5ee56350
Stepan <st.a.tarasenko@gmail.com>
2021-05-19 19:21:40 +0300

    add health check

--- lab/facade-service/src/main/java/com/example/facadeservice/demo/FacadeServiceApplication.java
+++ lab/facade-service/src/main/java/com/example/facadeservice/demo/FacadeServiceApplication.java
@@ -2,9 +2,20 @@ package com.example.facadeservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FacadeServiceApplication {
+    
+    @GetMapping("/actuator/health")
+    public ResponseEntity<String> myCustomCheck() {
+        String message = "Testing my health check function";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FacadeServiceApplication.class, args);
